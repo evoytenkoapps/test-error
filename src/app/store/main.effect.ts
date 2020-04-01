ac0dc5207fbcff6e7ae4fd0fafeb4d993ca5498f
Евгений Войтенко <evoytenkoapps@gmail.com>
2020-04-01 10:33:14 +0300

    add prettier

--- src/app/store/main.effect.ts
+++ src/app/store/main.effect.ts
@@ -1,14 +1,23 @@
-import { Injectable } from "@angular/core";
-import { Actions, Effect, ofType } from "@ngrx/effects";
+import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
 import {
   SEND_DATA,
   SendData,
   SendDataComplete,
   SendDataError
-} from "./actions";
-import { catchError, map, switchMap } from "rxjs/operators";
-import { HttpService } from "../service/http.service";
-import { of } from "rxjs";
+} from './actions';
+import {
+  catchError,
+  delay,
+  map,
+  mergeMap,
+  retry,
+  retryWhen,
+  shareReplay,
+  switchMap
+} from 'rxjs/operators';
+import { HttpService } from '../service/http.service';
+import { of } from 'rxjs';
 
 @Injectable()
 export class MainEffect {
@@ -20,7 +29,12 @@ export class MainEffect {
     switchMap(action =>
       this.httpService.sendData(action.payload).pipe(
         map(data => new SendDataComplete(action.payload)),
-        catchError(error => of(new SendDataError(action.payload)))
+        retryWhen(errors =>
+          errors.pipe(
+            delay(1000),
+            mergeMap(error => of(error))
+          )
+        )
       )
     )
   );
